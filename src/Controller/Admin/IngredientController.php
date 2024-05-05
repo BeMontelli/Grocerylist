f9af3b5c1a44d0d6e857419dceb6fb5cc6cb8a91
Benjamin.M <benjmonte@outlook.fr>
2024-05-05 19:20:03 +0200

    Added base ingredients

---
+++ src/Controller/Admin/IngredientController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+
+#[Route("/admin/ingredients", name: "admin.ingredient.")]
+class IngredientController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(IngredientRepository $ingredientRepository): Response
+    {
+        return $this->render('admin/ingredient/index.html.twig', [
+            'ingredients' => $ingredientRepository->findAll()
+        ]);
+    }
+
+    #[Route('/create/', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class,$ingredient);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($ingredient);
+            $entityManager->flush();
+            $this->addFlash('success', 'Ingredient saved !');
+            return $this->redirectToRoute('admin.ingredient.index');
+        }
+
+        return $this->render('admin/ingredient/create.html.twig',[
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{slug}-{id}', name: 'show', requirements: ['id' => Requirement::DIGITS, 'slug' => Requirement::ASCII_SLUG])]
+    public function show(string $slug, int $id, IngredientRepository $ingredientRepository): Response
+    {
+        return $this->render('admin/ingredient/show.html.twig', [
+            'ingredient' => $ingredientRepository->find($id),
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit', requirements: ['id' => Requirement::DIGITS], methods: ['GET','POST'])]
+    public function edit(Request $request, Ingredient $ingredient, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            $entityManager->persist($formData);
+            $entityManager->flush();
+            $this->addFlash('success', 'Ingredient updated !');
+            return $this->redirectToRoute('admin.ingredient.edit', ["id" => $ingredient->getId()]);
+        }
+
+        return $this->render('admin/ingredient/edit.html.twig', [
+            'ingredient' => $ingredient,
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', requirements: ['id' => Requirement::DIGITS], methods: ['DELETE'])]
+    public function delete(Ingredient $ingredient, EntityManagerInterface $entityManager) {
+        $entityManager->remove($ingredient);
+        $entityManager->flush();
+        $this->addFlash('success', 'Ingredient '.$ingredient->getTitle().' deleted !');
+        return $this->redirectToRoute('admin.ingredient.index');
+    }
+}
---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Sequentially([
+        new Assert\NotBlank(),
+        new Assert\Length(min: 4),
+    ])]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Sequentially([
+        new Assert\Length(min: 4),
+        new Assert\Regex(
+            pattern: "/^[a-z0-9]+(?:-[a-z0-9]+)*$/",
+            message: "The slug should only contain lowercase letters, numbers, and dashes, and should start and end with a letter or number."
+        ),
+    ])]
+    private ?string $slug = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
