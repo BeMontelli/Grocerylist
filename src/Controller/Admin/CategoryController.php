68dfaba0fc997b687b9274ca577de62e83ac787f
Benjamin.M <benjmonte@outlook.fr>
2024-05-05 15:11:20 +0200

    Added Category.php CRUD + migration

---
+++ src/Controller/Admin/CategoryController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+
+#[Route("/admin/categories", name: "admin.category.")]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('admin/category/index.html.twig', [
+            'categories' => $categoryRepository->findAll()
+        ]);
+    }
+
+    #[Route('/create/', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class,$category);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+            $this->addFlash('success', 'Recipe saved !');
+            return $this->redirectToRoute('admin.category.index');
+        }
+
+        return $this->render('admin/category/create.html.twig',[
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{slug}-{id}', name: 'show', requirements: ['id' => Requirement::DIGITS, 'slug' => Requirement::ASCII_SLUG])]
+    public function show(string $slug, int $id, CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('admin/category/show.html.twig', [
+            'category' => $categoryRepository->find($id),
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'edit', requirements: ['id' => Requirement::DIGITS], methods: ['GET','POST'])]
+    public function edit(Request $request, Category $category, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            $entityManager->persist($formData);
+            $entityManager->flush();
+            $this->addFlash('success', 'Recipe updated !');
+            return $this->redirectToRoute('admin.category.edit', ["id" => $category->getId()]);
+        }
+
+        return $this->render('admin/category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', requirements: ['id' => Requirement::DIGITS], methods: ['DELETE'])]
+    public function delete(Category $category, EntityManagerInterface $entityManager) {
+        $entityManager->remove($category);
+        $entityManager->flush();
+        $this->addFlash('success', 'Recipe '.$category->getTitle().' deleted !');
+        return $this->redirectToRoute('admin.category.index');
+    }
+}
---
+++ src/Form/CategoryType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title',TextType::class, [
+                'empty_data' => '',
+                'label' => 'Category title'
+            ])
+            ->add('slug',TextType::class, [
+                'empty_data' => '',
+                'required' => false,
+                'label' => 'Slug'
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save Category'
+            ])
+            ->addEventListener(FormEvents::PRE_SUBMIT,$this->autoSlug(...))
+            ->addEventListener(FormEvents::POST_SUBMIT,$this->autoTimestamps(...))
+        ;
+    }
+
+    public function autoSlug(PreSubmitEvent $event) : void {
+        $slugger = new AsciiSlugger();
+        $data = $event->getData();
+
+        $data['slug'] = strtolower($slugger->slug((!empty($data['slug'])) ? $data['slug'] : $data['title']));
+
+        $event->setData($data);
+    }
+
+    public function autoTimestamps(PostSubmitEvent $event) : void {
+        $data = $event->getData();
+        if(!$data instanceof Category) return;
+
+        if(!$data->getId()) $data->setCreatedAt(new \DateTimeImmutable());
+        $data->setUpdatedAt(new \DateTimeImmutable());
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
