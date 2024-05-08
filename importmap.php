4758e6dda408d031feb82ac466e5f660fe8f31e4
Benjamin.M <benjmonte@outlook.fr>
2024-05-08 20:42:35 +0200

    Base layout Front / Back

--- importmap.php
+++ importmap.php
@@ -16,6 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'appback' => [
+        'path' => './assets/appback.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
