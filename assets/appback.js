ae37c265fd4c38bb6341333113f48259297b42eb
Benjamin.M <benjmonte@outlook.fr>
2024-05-10 17:35:49 +0200

    site layout + bootstrap

--- assets/appback.js
+++ assets/appback.js
@@ -1,11 +1,13 @@
 import './bootstrap.js';
+import './bootstrap.bundle.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/boxicons.min.css';
+import './styles/boxicons.min.css'; // Boxicons
+import './styles/bootstrap.min.css'; // Bootstrap
 import './styles/appback.css';
 
 console.log('This log comes from assets/appback.js - welcome to AssetMapper! 🎉');
