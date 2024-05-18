37e1121a9d180ddb564d681dcdc84713f1cdb734
Benjamin.M <benjmonte@outlook.fr>
2024-05-18 20:21:07 +0200

    Add GroceryListType logic + fix styles

--- src/Form/GroceryListType.php
+++ src/Form/GroceryListType.php
@@ -4,23 +4,48 @@ namespace App\Form;
 
 use App\Entity\GroceryList;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class GroceryListType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('title',TextType::class, [
+                'empty_data' => '',
+                'label' => 'List title'
             ])
-            ->add('updatedAt', null, [
-                'widget' => 'single_text',
+            ->add('slug',TextType::class, [
+                'empty_data' => '',
+                'required' => false,
+                'label' => 'Slug'
             ])
+            ->addEventListener(FormEvents::PRE_SUBMIT,$this->autoSlug(...))
+            ->addEventListener(FormEvents::POST_SUBMIT,$this->autoTimestamps(...))
         ;
+    }
+
+    public function autoSlug(PreSubmitEvent $event) : void {
+        $slugger = new AsciiSlugger();
+        $data = $event->getData();
+
+        $data['slug'] = strtolower($slugger->slug((!empty($data['slug'])) ? $data['slug'] : $data['title']));
+
+        $event->setData($data);
+    }
+
+    public function autoTimestamps(PostSubmitEvent $event) : void {
+        $data = $event->getData();
+        if(!$data instanceof GroceryList) return;
+
+        if(!$data->getId()) $data->setCreatedAt(new \DateTimeImmutable());
+        $data->setUpdatedAt(new \DateTimeImmutable());
     }
 
     public function configureOptions(OptionsResolver $resolver): void
