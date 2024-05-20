d501d2be1de17d3fa64c9a34f0be7d5c6df681e4
Benjamin.M <benjmonte@outlook.fr>
2024-05-20 12:05:20 +0200

    Added admin base Sections

--- src/Form/SectionType.php
+++ src/Form/SectionType.php
@@ -2,52 +2,30 @@
 
 namespace App\Form;
 
-use App\Entity\Ingredient;
-use App\Entity\Recipe;
+use App\Entity\GroceryList;
 use App\Entity\Section;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Event\PreSubmitEvent;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\String\Slugger\AsciiSlugger;
 
-class IngredientType extends AbstractType
+class SectionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('title',TextType::class, [
                 'empty_data' => '',
-                'label' => 'Ingredient title'
+                'label' => 'List title'
             ])
             ->add('slug',TextType::class, [
                 'empty_data' => '',
                 'required' => false,
                 'label' => 'Slug'
-            ])
-            ->add('recipes', EntityType::class, [
-                'label' => 'Recipes related',
-                'class' => Recipe::class,
-                'choice_label' => 'title',
-                'multiple' => true,
-                'expanded' => true,
-                'by_reference' => false
-            ])
-            ->add('section', EntityType::class, [
-                'label' => 'Section',
-                'class' => Section::class,
-                'choice_label' => 'title',
-                'multiple' => false,
-                'expanded' => false,
-                'by_reference' => false
-            ])
-            ->add('save', SubmitType::class, [
-                'label' => 'Save Ingredient'
             ])
             ->addEventListener(FormEvents::PRE_SUBMIT,$this->autoSlug(...))
             ->addEventListener(FormEvents::POST_SUBMIT,$this->autoTimestamps(...))
@@ -65,7 +43,7 @@ class IngredientType extends AbstractType
 
     public function autoTimestamps(PostSubmitEvent $event) : void {
         $data = $event->getData();
-        if(!$data instanceof Ingredient) return;
+        if(!$data instanceof Section) return;
 
         if(!$data->getId()) $data->setCreatedAt(new \DateTimeImmutable());
         $data->setUpdatedAt(new \DateTimeImmutable());
@@ -74,7 +52,7 @@ class IngredientType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Ingredient::class,
+            'data_class' => Section::class,
         ]);
     }
 }
