3915fe2f1d8a971a65c61bdc2745d5ee88438da5
Benjamin.M <benjmonte@outlook.fr>
2024-05-23 14:12:11 +0200

    Fix Ingredient length assert to allow short title item

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -21,14 +21,14 @@ class Ingredient
     #[ORM\Column(length: 255)]
     #[Assert\Sequentially([
         new Assert\NotBlank(),
-        new Assert\Length(min: 4),
+        new Assert\Length(min: 3),
     ])]
     #[Groups(['ingredients.*','recipes.*'])]
     private ?string $title = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\Sequentially([
-        new Assert\Length(min: 4),
+        new Assert\Length(min: 3),
         new Assert\Regex(
             pattern: "/^[a-z0-9]+(?:-[a-z0-9]+)*$/",
             message: "The slug should only contain lowercase letters, numbers, and dashes, and should start and end with a letter or number."
