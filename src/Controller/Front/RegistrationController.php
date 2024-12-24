4018057f53717ee370a36ca218514591dea50be8
Benjamin.M <benjmonte@outlook.fr>
2024-12-24 11:09:02 +0100

    deactivate registration emailVerifier

--- src/Controller/Front/RegistrationController.php
+++ src/Controller/Front/RegistrationController.php
@@ -50,13 +50,13 @@ class RegistrationController extends AbstractController
             $this->dataImportService->importData($user);
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            /*$this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
                     ->from(new Address('do-not-answer@grocerylist.fr', 'GroceryList'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
+            );*/
 
             // do anything else you need here, like send an email
 
