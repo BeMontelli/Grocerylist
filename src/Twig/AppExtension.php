0a607109804b5c12161dbfbf1c43aefa46b1b3e4
Benjamin.M <benjmonte@outlook.fr>
2025-01-13 17:05:27 +0100

    Added twig filter truncate()

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('truncate', [$this, 'truncate']),
+        ];
+    }
+
+    public function truncate(string $text, int $maxLength = 20): string
+    {
+        if (mb_strlen($text) <= $maxLength) {
+            return $text;
+        }
+
+        return mb_substr($text, 0, $maxLength - 3) . '...';
+    }
+}
